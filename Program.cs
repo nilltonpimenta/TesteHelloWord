62a87ef8693a22bebb6ccf6e1f3a823b536de6d3
Jullio <njqp@icomp.ufam.edu.br>
2022-02-16 02:09:39 -0400

    commit

--- Program.cs
+++ Program.cs
@@ -5,5 +5,5 @@ Console.WriteLine("What is your name?");
 var name = Console.ReadLine();
 var currentDate = DateTime.Now;
 Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.Write($"{Environment.NewLine}Press any key to exit... Bye");
 Console.ReadKey(true);
